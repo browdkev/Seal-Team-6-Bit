91ee7a7e23824746f13a00af5f7c0791fe4f63f1
Kevin Browder <browderkevin54@gmail.com>
2017-02-20 22:59:45 -0700

    Added Animations and basic movement script (Removed excess files)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 50f;
+    public float jumpPower = 150f;
+
+    public bool grounded;
+
+    private Rigidbody2D rb2d;
+
+	void Start ()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce((Vector2.right * speed) * h);
+    }
+}
