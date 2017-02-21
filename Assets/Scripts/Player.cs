42df33c0d9edd98a34190b999f528074eff187b0
Kevin Browder <browderkevin54@gmail.com>
2017-02-21 16:11:10 -0700

    Added more animations and movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float maxSpeed = 5;
     public float speed = 50f;
     public float jumpPower = 150f;
 
     public bool grounded;
 
     private Rigidbody2D rb2d;
+    private Animator anim;
 
 	void Start ()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
-	}
+        anim = gameObject.GetComponent<Animator>();
+    }
 	
 	void Update ()
     {
-		
-	}
+        anim.SetBool("Grounded", grounded);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
     void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
 
         rb2d.AddForce((Vector2.right * speed) * h);
+
+        if(rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
     }
 }
