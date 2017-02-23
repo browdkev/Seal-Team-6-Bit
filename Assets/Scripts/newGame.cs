ede9d389b3938e1af545ea89f069d36079e9bb60
IceBoxWillys <janderson5463@gmail.com>
2017-02-22 22:52:46 -0700

    WORKING UI THINGYS

---
+++ Assets/Scripts/newGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newGame : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void newScene()
+    {
+
+    }
+    public void onClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("sceneMain");
+    }
+}
