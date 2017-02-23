c768ad62487029207b4759e387a35fae0b745127
Nathanstouffer <nathanstouffer1999@gmail.com>
2017-02-22 23:48:47 -0700

    Streamwriter Script

---
+++ Assets/Scripts/StreamwriteInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.IO;
+
+public class StreamwriteInitializer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        string fileLocation = System.Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory);
+
+        StreamWriter createLogFileWriter = new StreamWriter(System.IO.Path.Combine(@fileLocation, "Seal Team 6-Bit Game Log.txt"), false);
+        createLogFileWriter.Close();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
