6863ebb36c174a5ca46947a200f3828940d3f1f0
IceBoxWillys <janderson5463@gmail.com>
2017-02-22 20:52:25 -0700

    Created Title Screen and Anim

---
+++ Assets/Scripts/cameraPan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraPan : MonoBehaviour {
+    public Animation anim;
+
+    void Start ()
+    {
+        anim.wrapMode = WrapMode.ClampForever;
+        anim.Play("cameraPan.anim");
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+
+    }
+}
